ddba1eef47c7b605806bd27956a7b5007b1ea393
Puri12 <tkdqja9812@gmail.com>
2022-11-14 10:04:41 +0900

    :tada: init: 프로젝트 시작

---
+++ src/main/java/com/baedal/dutchpay/DutchPayApplication.java
@@ -0,0 +1,13 @@
+package com.baedal.dutchpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DutchPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DutchPayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/baedal/dutchpay/DutchPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baedal.dutchpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DutchPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
